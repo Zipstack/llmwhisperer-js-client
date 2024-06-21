75bf5de07fbcfe6abfbacbd8609a5009b1787a06
jaseemjaskp <jaseem@zipstack.com>
2024-06-21 20:06:11 +0530

    test script added with jest

--- test/test.js
+++ test/test.js
@@ -1,49 +1,78 @@
-var LLMWhispererClient = require('../index').LLMWhispererClient;
+const fs = require('fs');
+const path = require('path');
+const LLMWhispererClient = require('../index').LLMWhispererClient;
+const client = new LLMWhispererClient({apiKey:process.env.LLMWHISPERER_API_KEY});
+describe('LLMWhispererClient', () => {
+  test('get_usage_info', async () => {
+    const usage_info = await client.getUsageInfo();
+    console.info(usage_info);
+    expect(typeof usage_info).toBe('object');
+    const expected_keys = [
+      'current_page_count',
+      'daily_quota',
+      'monthly_quota',
+      'overage_page_count',
+      'subscription_plan',
+      'today_page_count',
+    ];
+    expect(Object.keys(usage_info)).toEqual(expect.arrayContaining(expected_keys));
+  });
 
-const client = new LLMWhispererClient();
+  const test_cases = [
+    ['ocr', 'line-printer', 'restaurant_invoice_photo.pdf'],
+    ['ocr', 'line-printer', 'credit_card.pdf'],
+    ['ocr', 'line-printer', 'handwritten-form.pdf'],
+    ['ocr', 'text', 'restaurant_invoice_photo.pdf'],
+    ['text', 'line-printer', 'restaurant_invoice_photo.pdf'],
+    ['text', 'text', 'handwritten-form.pdf'],
+  ];
 
+  test.each(test_cases)(
+    'whisper(%s, %s, %s)',
+    async (processing_mode, output_mode, input_file) => {
+      const data_dir = path.join(__dirname, 'data');
+      const file_path = path.join(data_dir, input_file);
+      const response = await client.whisper({processingMode:processing_mode, outputMode:output_mode, filePath:file_path, timeout: 200 });
+      console.debug(response);
 
-(async () => {
-    // usage_info = await client.getUsageInfo();
-    // console.log(usage_info);
-    //whisper = await client.whisper({filePath: 'sample_files/restaurant_invoice_photo.pdf'});
-    //whisper = await client.whisper({url: 'https://storage.googleapis.com/pandora-static/samples/bill.jpg.pdf'});
-    // whisper = await client.whisper({
-    //     filePath: 'sample_files/credit_card.pdf',
-    //     processingMode: 'text',
-    //     forceTextProcessing: true,
-    //     pagesToExtract: '1-2',
-    // });    
-    //console.log(whisper);
+      const exp_basename = `${path.parse(input_file).name}.${processing_mode}.${output_mode}.txt`;
+      const exp_file = path.join(data_dir, 'expected', exp_basename);
+      const exp = await fs.promises.readFile(exp_file, 'utf-8');
 
-    //6479fc48|ba4473ee92b30823c4ed3da759ef670f
-    //whisper_status = await client.whisperStatus('6479fc48|ba4473ee92b30823c4ed3da759ef670f');
-    //console.log(whisper_status);
+      expect(typeof response).toBe('object');
+      expect(response.statusCode).toBe(200);
+      // expect(response.extracted_text).toBe(exp);
+    },
+    200000);
 
-    whisper = await client.whisper({
-        filePath: 'sample_files/credit_card.pdf',
-        timeout: 1,
-        storeMetadataForHighlighting: true,
-    });
-    //Keep checking the status until it is completed
-    statusX = whisper.status;
-    while (statusX === 'processing') {
-        console.log('Processing... '+whisper['whisper-hash']);
-        await new Promise(r => setTimeout(r, 3000));
-        whisperStatus = await client.whisperStatus(whisper['whisper-hash']);
-        statusX = whisperStatus.status;
-    }
-    if (statusX === 'processed') {
-        //Retrieve the result
-        whisper = await client.whisperRetrieve(whisper['whisper-hash']);
-        console.log(whisper);
-    } else {
-        console.log('Error');
-    }
+  // TODO: Review and port to Jest based tests
+  test.skip('whisper', () => {
+    // response = client.whisper(
+    //   'https://storage.googleapis.com/pandora-static/samples/bill.jpg.pdf'
+    // );
+    const response = client.whisper(
+      'test_files/restaurant_invoice_photo.pdf',
+      { timeout: 200, store_metadata_for_highlighting: true },
+    );
+    console.info(response);
+    // expect(typeof response).toBe('object');
+  });
 
-    //41ebb056|ba4473ee92b30823c4ed3da759ef670f
-    highlights = await client.highlightData('41ebb056|ba4473ee92b30823c4ed3da759ef670f', 'Pay by Computer');
-    console.log(highlights);
+  test.skip('whisper_status', () => {
+    const response = client.whisper_status('7cfa5cbb|5f1d285a7cf18d203de7af1a1abb0a3a');
+    console.info(response);
+    expect(typeof response).toBe('object');
+  });
 
-})();
+  test.skip('whisper_retrieve', () => {
+    const response = client.whisper_retrieve('7cfa5cbb|5f1d285a7cf18d203de7af1a1abb0a3a');
+    console.info(response);
+    expect(typeof response).toBe('object');
+  });
 
+  test.skip('whisper_highlight_data', () => {
+    const response = client.highlight_data('9924d865|5f1d285a7cf18d203de7af1a1abb0a3a', 'Indiranagar');
+    console.info(response);
+    expect(typeof response).toBe('object');
+  });
+});
